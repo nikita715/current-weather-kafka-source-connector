c9e70824dde8c3ec3037453a24f83d92194d74fb
Nikita Stepochkin <nikstepbox@gmail.com>
2023-08-29 01:45:51 +0300

    Use string value

--- src/main/kotlin/ru/nikstep/kafka/connect/random/RandomSourceTask.kt
+++ src/main/kotlin/ru/nikstep/kafka/connect/random/RandomSourceTask.kt
@@ -1,16 +1,13 @@
 package ru.nikstep.kafka.connect.random
 
 import org.apache.kafka.common.config.AbstractConfig
-import org.apache.kafka.connect.data.Schema
-import org.apache.kafka.connect.data.SchemaBuilder
-import org.apache.kafka.connect.data.Struct
+import org.apache.kafka.connect.data.Schema.STRING_SCHEMA
 import org.apache.kafka.connect.source.SourceRecord
 import org.apache.kafka.connect.source.SourceTask
 import java.time.Instant
 import java.time.OffsetDateTime
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
-
 
 class RandomSourceTask : SourceTask() {
     private lateinit var prefix: String
@@ -49,35 +46,14 @@ class RandomSourceTask : SourceTask() {
                 sourceOffset(),
                 topic,
                 null,
-                KEY_SCHEMA,
-                buildRecordKey(Random.nextInt(100).toString()),
-                VALUE_SCHEMA,
-                buildRecordValue(Random.nextInt(100).toString()),
+                null,
+                null,
+                STRING_SCHEMA,
+                Random.nextInt(100).toString(),
                 Instant.now().epochSecond
             )
         )
     }
 
     override fun stop() {}
-
-    private fun buildRecordKey(key: String): Struct =
-        Struct(KEY_SCHEMA)
-            .put("key", key)
-
-    private fun buildRecordValue(value: String): Struct =
-        Struct(VALUE_SCHEMA)
-            .put("value", prefix + value)
-
-    companion object {
-
-        var KEY_SCHEMA: Schema = SchemaBuilder.struct().name("key-schema")
-            .version(1)
-            .field("key", Schema.STRING_SCHEMA)
-            .build()
-
-        var VALUE_SCHEMA: Schema = SchemaBuilder.struct().name("value-schema")
-            .version(1)
-            .field("value", Schema.STRING_SCHEMA)
-            .build()
-    }
 }
